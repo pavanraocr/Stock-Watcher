31a8cfb4acdfe59b7938e008d969b4838f14748a
pavanraocr <pavanraocr@gmail.com>
2017-03-06 00:06:31 -0600

    first version:

---
+++ Stock-Monitor/app/src/main/java/com/example/pavan/stock_monitor/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.pavan.stock_monitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        return super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
+        return true;
+    }
+
+
+}
